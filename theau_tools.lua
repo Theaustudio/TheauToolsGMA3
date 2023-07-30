593aff350bcd3954b6b1a0bdf2b65e8f2810701a
Theaustudio <tluclefe@gmail.com>
2023-07-30 19:40:50 +0200

    add images

--- theau_tools.lua
+++ theau_tools.lua
@@ -9,24 +9,32 @@ local function main(displayHandle)
     --     backColor = "Assignment.Plugin"
     -- })
 
-    local toolSelectBox = MessageBox(
-        {
-            title = 'Theau Tools',
-            message = 'Please Select a Tool',
-            commands = {{value = 1, name = "Preset Picker Generator"}, {value = 2, name = "MAtrix picker generator"}, {value = 3, name = "Color picker generator"},},
-            backColor = "Assignment.Plugin",
-            timeout = 10000,
-            timeoutResultCancel = true,
-            icon = "wizard",
-        })
-    if toolSelectBox.success == false then return end
+    local toolSelectBox = MessageBox({
+        title = 'Theau Tools',
+        message = 'Please Select a Tool',
+        commands = {{
+            value = 1,
+            name = "Preset Picker Generator"
+        }, {
+            value = 2,
+            name = "MAtrix picker generator"
+        }, {
+            value = 3,
+            name = "Color picker generator"
+        }},
+        backColor = "Assignment.Plugin",
+        timeout = 10000,
+        timeoutResultCancel = true,
+        icon = "wizard"
+    })
+    if toolSelectBox.success == false then
+        return
+    end
 
-    if toolSelectBox.result == 3 then
-        Cmd("plugin ThéauTools.ColorPickerGenerator")
+    if toolSelectBox.result == 1 then
+        Cmd("plugin ThéauTools.PresetPickerGenerator")
     elseif toolSelectBox.result == 2 then
         Cmd("plugin ThéauTools.MAtricksPickerGenerator")
-    elseif toolSelectBox.result == 1 then
-        Cmd("plugin ThéauTools.PresetPickerGenerator")
     end
 end
 
